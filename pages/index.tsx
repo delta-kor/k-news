e903c7b62139263a1a880e2e3a5edece254f24ce
delta-kor <deltakbot1@gmail.com>
2020-12-31 19:54:17 +0900

    Add section heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
 import Header from '../components/molecules/header';
 
+const Heading = styled.div`
+  position: absolute;
+  height: 35px;
+  left: 24px;
+  right: 24px;
+  top: 91px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 35px;
+  color: #112254;
+`;
+
 const IndexPage: NextPage = () => {
-  return <Header content={'K-NEWS'} />;
+  return (
+    <div>
+      <Header content={'K-NEWS'} />
+      <Heading>헤드라인</Heading>
+    </div>
+  );
 };
 
 export default IndexPage;
