aed189fc925178c451e3c94768a15bbe16b5a956
delta-kor <deltakbot1@gmail.com>
2020-12-31 15:40:25 +0900

    Add header

---
+++ components/molecules/header.tsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import Logo from '../atoms/logo';
+
+interface Props {
+  content: string;
+}
+
+const Layout = styled.div`
+  width: 100%;
+  height: 91px;
+`;
+
+const Icon = styled.div`
+  position: absolute;
+  width: 43px;
+  height: 43px;
+  left: 24px;
+  top: 24px;
+`;
+
+const Content = styled(motion.p)`
+  position: absolute;
+  width: 260px;
+  height: 35px;
+  left: 91px;
+  top: 29px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 35px;
+  color: #112254;
+`;
+
+export default class Header extends Component<Props, any> {
+  render() {
+    return (
+      <Layout>
+        <Icon>
+          <Logo />
+        </Icon>
+        <Content layoutId={'title'}>{this.props.content}</Content>
+      </Layout>
+    );
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import Header from '../components/molecules/header';
+
+export default function IndexPage() {
+  return <Header content={'K-NEWS'} />;
+}
